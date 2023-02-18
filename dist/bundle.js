59d3345e0f93d0e52e8985f6f837883c29ecc9b8
Odi000 <mandimandi.ro@gmail.com>
2023-02-18 16:18:28 +0100

    Files are connected and working

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+console.log('Its working');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsMkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9tYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdJdHMgd29ya2luZycpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     context: path.resolve(__dirname, 'src'),
-    entry: 'main.js',
+    entry: './main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
