f578777fd1e69a171eb828546ccdcc186cb1e104
Odi000 <mandimandi.ro@gmail.com>
2023-03-17 14:32:21 +0200

    Sidebar work

--- src/main.js
+++ src/main.js
@@ -17,6 +17,11 @@ const sideBarBtn = document.querySelector('.list');
 sideBarBtn.onclick = () => {
     const sideBar = document.querySelector('.sidebar');
     sideBar.style.display = 'block';
-    setTimeout(()=> sideBar.classList.add('active'), 10);
+    setTimeout(()=> sideBar.classList.add('opening'), 10);
+    sideBar.addEventListener('transitionend', (e)=> {
+        if (e.propertyName === 'background-color') {
+            sideBar.classList.remove('opening');
+            sideBar.classList.add('opened');
+        };
+    })
 }
-console.log(sideBarBtn)
