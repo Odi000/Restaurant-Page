1e5ccff41d4df1ccdf3a5bcca7b3a580f4f46ecf
Odi000 <mandimandi.ro@gmail.com>
2023-03-18 21:37:43 +0200

    Code messup

--- src/main.js
+++ src/main.js
@@ -11,17 +11,3 @@ function linkFontAwesome() {
     link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css";
     return link;
 }
-
-const sideBarBtn = document.querySelector('.list');
-
-sideBarBtn.onclick = () => {
-    const sideBar = document.querySelector('.sidebar');
-    sideBar.style.display = 'block';
-    setTimeout(()=> sideBar.classList.add('opening'), 10);
-    sideBar.addEventListener('transitionend', (e)=> {
-        if (e.propertyName === 'background-color') {
-            sideBar.classList.remove('opening');
-            sideBar.classList.add('opened');
-        };
-    })
-}
