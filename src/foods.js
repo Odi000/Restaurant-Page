b322c60bd5ca89149c99b25c1fd19e3e569c89c4
Odi000 <mandimandi.ro@gmail.com>
2023-04-06 01:33:43 +0300

    food arrays added

--- src/foods.js
+++ src/foods.js
@@ -69,6 +69,74 @@ const salads = [
     new Meal('MANGO SALAD', `Spicy green mango salad topped with pork, 
     dried shrimp & peanuts`, dairyFree, glutenFree, _500cal)
 ]
+
+const curryRice = [
+    new Meal('CHICKEN CURRY', `Rich, fragrant Vietnamese curry with 
+    veggies & mushrooms, served with broken rice & chicken`, dairyFree, glutenFree),
+    new Meal('BRISKET CURRY', `Rich, fragrant Vietnamese curry with 
+    veggies & mushrooms, served with broken rice & beef brisket`, dairyFree, glutenFree),
+    new Meal('PRAWN CURRY', `Rich, fragrant Vietnamese curry with 
+    veggies & mushrooms, served with broken rice & king prawns`, dairyFree, glutenFree),
+    new Meal('TOFU & MUSHROOM CURRY', `Rich, fragrant Vietnamese 
+    curry with veggies & mushrooms, served with broken rice & tofu`, vegan, vegetarian, dairyFree, glutenFree),
+    new Meal("THIS ISN'T CHICKEN CURRY", `Rich, fragrant Vietnamese 
+    curry with veg & mushrooms, served with rice & vegan THIS isn't chicken`),
+    new Meal('CHICKEN FRIED RICE', `Aromatic, spicy wok-fried broken 
+    rice with chicken & dried shrimp with option to add a fried egg`, dairyFree, glutenFree),
+    new Meal('MUSHROOM FRIED RICE', `Spicy wok-fried broken rice with 
+    shiitake mushroom & Thai basil with option to add a fried egg`, vegan, vegetarian, dairyFree, glutenFree),
+    new Meal('VEGAN CHICKEN FRIED RICE', `Spicy wok-fried broken rice 
+    with vegan THIS isn't chicken`),
+    new Meal('PORK RICE BOWL', `Rice bowls topped with loads of 
+    veg & herbs, served with chargrilled pork`, dairyFree, glutenFree),
+    new Meal('BEEF RICE BOWL', `Rice bowls topped with loads of 
+    veg & herbs, served with beef in betal leaf`, dairyFree, glutenFree)
+];
+
+const wokFriedNoodles = [
+    new Meal('CHICKEN NOODLES', `Wok-fried noodles with all the 
+    trimmings, topped with chicken`, dairyFree, glutenFree, _500cal),
+    new Meal('BEEF NOODLES', `Wok-fried noodles with all the 
+    trimmings, topped with beef br/isket`, dairyFree, glutenFree, _500cal),
+    new Meal('CHICKEN & PRAWN NOODLES', `Wok-fried noodles with 
+    all the trimmings, topped with chicken & prawns`, dairyFree, glutenFree, _500cal),
+    new Meal('TOFU & MUSHROOM NOODLES', `Wok-fried noodles with 
+    all the trimmings, topped with tofu & mushrooms`, ...all),
+    new Meal('VEGAN CHICKEN NOODLES', `Wok-fried noodles with 
+    all the trimmings, topped with vegan THIS isn't chicken noodles`, vegan, vegetarian, dairyFree, glutenFree)
+];
+
+const vermicelli = [
+    new Meal('BÚN CHICKEN', `Vermicelli rice noodles with a lemongrass 
+    and chilli wok-fried topping, served with chicken`, dairyFree, _500cal),
+    new Meal('BÚN BEEF', `Vermicelli rice noodles with a lemongrass and chilli 
+    wok-fried topping, served with beef`, dairyFree,),
+    new Meal('BÚN KING PRAWN', `Vermicelli rice noodles with a lemongrass and 
+    chilli wok-fried topping, served with king prawns`, dairyFree, _500cal),
+    new Meal('BÚN NEM NUONG PORK BALLS', `Vermicelli rice noodles with a lemongrass 
+    and chilli wok-fried topping, served with pork meatballs`, dairyFree,),
+    new Meal('BÚN TOFU & MUSHROOM', `Vermicelli rice noodles with a lemongrass and 
+    chilli wok-fried topping, served with tofu & mushroom`, vegan, vegetarian, dairyFree, _500cal),
+    new Meal('BÚN VEGGIE SPRING ROLLS', `Vermicelli rice noodles with a lemongrass 
+    and chilli wok-fried topping, served with spring rolls`, vegan, vegetarian, dairyFree, _500cal)
+];
+
+const sides = [
+    new Meal('PRAWN CRACKERS', 'with sweet chilli sauce', dairyFree, glutenFree, _500cal),
+    new Meal("'PRAWN-LESS CRACKERS'", 'with sweet chilli sauce', ...all),
+    new Meal('"PRAWNLESS" CRACKERS', 'with sweet chilli sauce', ...all),
+    new Meal('STIR FRIED SPINACH', 'Stir fried morning glory (water spinach) in garlic', ...all),
+    new Meal('CHINESE LEAF', 'Stir fried Chinese leaf in soy sauce', ...all)
+];
+
+const desserts = [
+    new Meal('CRISPY BANANA FRITTER', `Banana fritter, as is or with ice cream (coconut or honey & ginger)`, vegan, vegetarian, dairyFree, glutenFree),
+    new Meal('PANDAN WAFFL', `Freshly cooked pandan waffle served with ice cream or sorbet`, glutenFree),
+    new Meal('CHOCOLATE TRUFFLE SLICE', `With ice cream or sorbet`),
+    new Meal('VIETNAMESE AFFOGATO', `Hot Vietnamese filter coffee to pour over a scoop of condensed milk ice cream`),
+    new Meal('FRESH FRUIT SORBET', `delicious fresh fruit sorbets from La Maison des Sorbets. raspberry or mango`, vegan),
+    new Meal('ICE CREAM', `Rich, sumptuous dairy ice creams made with whole milk & double cream.`, glutenFree),
+];
 //---
 
 starterMeals.forEach((meal) => putMealInDOM(meal));
@@ -78,13 +146,29 @@ categoryList.onchange = () => {
     const category = categoryList.value;
     foods.innerHTML = ""; // I can remove child nodes with an interval one by one
 
-    if (category == "starters") {
-        starterMeals.forEach((meal) => putMealInDOM(meal));
-    } if (category == "salads") {
-        salads.forEach((meal) => putMealInDOM(meal));
+    switch(category) {
+        case "starters":
+            starterMeals.forEach((meal) => putMealInDOM(meal));
+            break;
+        case "salads":
+            salads.forEach((meal) => putMealInDOM(meal));
+            break;
+        case "curry&rice":
+            curryRice.forEach((meal)=> putMealInDOM(meal));
+            break;
+        case "wok-fried_noodles":
+            wokFriedNoodles.forEach((meal)=> putMealInDOM(meal));
+            break;
+        case "vermicelli_noodles":
+            vermicelli.forEach((meal)=> putMealInDOM(meal));
+            break;
+        case "sides":
+            sides.forEach((meal)=> putMealInDOM(meal));
+            break;
+        case "desserts":
+            desserts.forEach((meal)=> putMealInDOM(meal));
     }
 }
-
 
 function putMealInDOM(meal) {
     const div = document.createElement('div');
