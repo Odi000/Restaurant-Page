817d298df4c10f3c7de553f674ee72689e19ead0
Odi000 <mandimandi.ro@gmail.com>
2023-04-30 00:47:12 +0200

    changed dist folder name

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './main.js',
     devtool: 'inline-source-map',
     devServer: {
-        static: './dist',
+        static: './docs',
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -17,7 +17,7 @@ module.exports = {
     ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
     },
     module: {
